2fc62bcccde09fefdb099ee4517c309928877d9e
jeetu1980 <217350592+jeetu1980@users.noreply.github.com>
2025-06-23 00:27:17 +0530

    naming and vaiables

---
+++ Basics/variables.go
@@ -0,0 +1,18 @@
+package basics
+
+import "fmt"
+
+var middleName = "Kumar"
+
+func main() {
+
+	// var count int
+	// var name string = "Amit"
+	// var name2 = "Kumar"
+
+	// count2 := 20
+	// lastName := "Tomar"
+
+	fmt.Println(middleName)
+
+}
